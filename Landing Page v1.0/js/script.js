0d00e138f065b8493079cd29432fb458f9e5844f
WallaceCampista <98376621+WallaceCampista@users.noreply.github.com>
2024-02-16 11:51:18 -0300

    Add files via upload

---
+++ Landing Page v1.0/js/script.js
@@ -0,0 +1,40 @@
+// Configurando funcionalidade do icone olho (revelhar senha)
+const forms= document.querySelector(".forms"),
+    pwShowHide = document.querySelectorAll(".eye-icon");
+
+    
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+        pwFields.forEach(password => {
+            if(password.type === "password"){   
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})
+
+// Configurando avanço automatico de imagens
+let count = 1;
+document.getElementById("radio1").checked = true
+
+setInterval( function(){
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
